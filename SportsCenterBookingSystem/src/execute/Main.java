f3b07cbe5a9d509ce817172fe466af4ff4336a4f
NG Wing Hei <wingheing9-c@my.cityu.edu.hk>
2024-10-01 11:17:48 +0800

    Read file

--- SportsCenterBookingSystem/src/execute/Main.java
+++ SportsCenterBookingSystem/src/execute/Main.java
@@ -2,12 +2,15 @@ package execute;
 
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
 
 import execute.BookingOfDay;
 
 public class Main {
 	public static void main(String[] args) {
 		ArrayList<BookingOfDay> allBookings = new ArrayList<>();
+		allBookings = getBookingsFromFile();
 		
 		Scanner scanner = new Scanner(System.in);
 		String action = "";
@@ -18,7 +21,14 @@ public class Main {
 		
 		switch (action) {
 			case "m":
-                System.out.println("Make booking");
+                System.out.println("Please input the date of booking (yyyymmdd):");
+                String date = scanner.nextLine();
+                System.out.println("Please input the starting time of booking (hh):");
+                int startTime = scanner.nextInt();
+                System.out.println("Please input the ending time of booking (hh):");
+                int endTime = scanner.nextInt();
+                
+                
                 break;
             case "c":
 				System.out.println("Change booking");
@@ -33,5 +43,36 @@ public class Main {
 				System.out.println("Quit");
 				break;
 		}
+	}
+
+	public static ArrayList<BookingOfDay> getBookingsFromFile() {
+		ArrayList<BookingOfDay> bookings = new ArrayList<>();
+		// Read from file
+		try {
+			File file = new File("./assets/booking_data.txt");
+			Scanner scanner = new Scanner(file);
+			while (scanner.hasNextLine()) {
+				String data = scanner.nextLine();
+				String[] bookingData = data.split(" ");
+				String date = bookingData[2];
+				
+				//????
+				BookingOfDay bookingOfDay = new BookingOfDay(date);
+				
+				
+				
+				String bookingID = bookingData[5];
+				int startTime = Integer.parseInt(bookingData[3]);
+				int endTime = Integer.parseInt(bookingData[4]);
+				Booking booking = new Booking(bookingID, startTime, endTime);
+				bookingOfDay.addBooking(booking);
+			}
+			scanner.close();
+		
+		} catch (FileNotFoundException e) {
+			System.out.println("An error occurred.");
+			e.printStackTrace();
+		}
+		return bookings;
 	}
 }
