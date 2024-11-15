8f6c76ac0493da4b5fcaab2e3614d748056ca139
WONG Ka In Karin <kikwong5-c@my.cityu.edu.hk>
2024-11-15 17:10:10 +0800

    remove isLeapYear

--- SportsCenterBookingSystem/src/execute/DateAndTime.java
+++ SportsCenterBookingSystem/src/execute/DateAndTime.java
@@ -60,13 +60,7 @@ public class DateAndTime {
 	}
 	
 	private static boolean isLeapYear(int year) {
-		if (year%400==0) {
-			return true;
-		}
-		else if (year%100==0) {
-			return false;
-		}
-		else if (year%4==0) {
+		if (year%4==0) {
 			return true;
 		}
 		else {
