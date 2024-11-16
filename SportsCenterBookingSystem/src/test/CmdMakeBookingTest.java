73c6fe69ab3ec76d85506c65ae983398c639750d
wzhang682 <160386127+wzhang682@users.noreply.github.com>
2024-11-16 17:56:45 +0800

    Add files via upload

--- SportsCenterBookingSystem/src/test/CmdMakeBookingTest.java
+++ SportsCenterBookingSystem/src/test/CmdMakeBookingTest.java
@@ -24,12 +24,33 @@ public class CmdMakeBookingTest {
     public void testExecuteMakeBooking_Success() {
     	   	User user = new User("001", "A", "123456");
 	        UserSessionManager.getInstance().setCurrentUser(user);
-	        String inputString = "001\n6\nN";
+	        String inputString = "001\n1\n241001 15-20\nN";
 	        Scanner scanner = new Scanner(inputString);
 
-	        CmdMakeBookingTest CmdMakeBookingTest = new CmdMakeBookingTest();
-	        CmdMakeBookingTest.execute(scanner);
+	        CmdMakeBooking CmdMakeBooking = new CmdMakeBooking();
+	        CmdMakeBooking.execute(scanner);
     }
+    @Test
+    public void testExecuteMakeBooking_InvalidRoom() {
+    	   	User user = new User("001", "A", "123456");
+	        UserSessionManager.getInstance().setCurrentUser(user);
+	        String inputString = "001\n6\n1\n241001 15-20\nN";
+	        Scanner scanner = new Scanner(inputString);
+
+	        CmdMakeBooking CmdMakeBooking = new CmdMakeBooking();
+	        CmdMakeBooking.execute(scanner);
+    }
+    @Test
+    public void testExecuteMakeBooking_InvalidDate() {
+    	   	User user = new User("001", "A", "123456");
+	        UserSessionManager.getInstance().setCurrentUser(user);
+	        String inputString = "001\n1\nn241003 15-20\n241001 15-20\nN";
+	        Scanner scanner = new Scanner(inputString);
+
+	        CmdMakeBooking CmdMakeBooking = new CmdMakeBooking();
+	        CmdMakeBooking.execute(scanner);
+    }
+
 
 
 }
