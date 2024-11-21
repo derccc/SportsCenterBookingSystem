b110654e48522c53c0e640e31070e805c7830736
derccc <107456273+derccc@users.noreply.github.com>
2024-11-21 11:35:27 +0800

    Update Common.java

--- SportsCenterBookingSystem/src/execute/Common.java
+++ SportsCenterBookingSystem/src/execute/Common.java
@@ -6,11 +6,16 @@ import java.util.Locale;
 import java.util.Scanner;
 
 public class Common {
+
+
+
     public enum InputType{
 		COMMAND("command"),
         DATE("date"),
         USERROLE("user role"),
-        PRICE("price");
+        PRICE("price"),
+        PASSWORD("password"),
+        USERID("user id");
 		
 		private String name;
 
@@ -29,7 +34,7 @@ public class Common {
             // Check if the input matches any of the valid commands
             for (String validCommand : validCommands) {
                 if (input.equalsIgnoreCase(validCommand)) {
-                    return input;
+                    return input.toLowerCase();
                 }
             }
             System.out.printf("Invalid %s, please input again.\n", type.getName());
@@ -39,7 +44,8 @@ public class Common {
     public static String getValidDate(Scanner scanner) {
         String input;
         while (true) {
-            input = scanner.nextLine().trim();
+            input = scanner.nextLine();
+            System.out.print(input);
 
             if (DateAndTime.isDateValid(input)) {
                 return input;
@@ -95,6 +101,39 @@ public class Common {
         }
     }
 
+    public static String getValidRoomTypeAndPrice(Scanner scanner) {
+
+        String message = "";
+        int price;
+
+        while (true) {
+
+            String input = scanner.nextLine().trim();
+            String[] splitInput = input.split("\\s+"); // Split input by spaces
+
+            if (splitInput.length != 2) {
+                message = "Input does not match the format";
+            } else if (!splitInput[1].matches("-?\\d+")) {
+                message = "Price must be numeric";
+            } else {
+                try {
+
+                    price = Integer.parseInt(splitInput[1]);
+
+                    if(price<=0){message = "Price must be larger than zero";}
+                    else{return input;} // Valid year and month}
+                        
+                    
+                } catch (NumberFormatException e) {
+                    message = "Error parsing input as numbers";
+                }
+            }
+
+            System.out.printf("%s, please input again.\n", message);
+        }
+    }
+
+
 
     public static int getValidPositiveNumber(Scanner scanner, InputType type) {
         String input;
@@ -123,7 +162,18 @@ public class Common {
         }
     }
     
+    public static String getValidInputLength(Scanner scanner, InputType type, int length) {
+        String input;
+        while (true) {
+            input = scanner.nextLine().trim();
 
+            if (!input.contains(" ") && input.length()>=length) {
+                return input;
+            }else
+            System.out.printf("The %s length should not be less than %d digits and space is not allowed.\n", type.getName(), length);
+            
+            }
+    }
 
 	
 	public static String formatDate(String orginalDate) {
