00b2e41e94319e1fa6e008f4b262b0c36386468c
derccc <107456273+derccc@users.noreply.github.com>
2024-11-21 11:42:09 +0800

    Add files via upload

--- SportsCenterBookingSystem/src/test/DateAndTimeTest.java
+++ SportsCenterBookingSystem/src/test/DateAndTimeTest.java
@@ -1,5 +1,6 @@
 package test;
 
+import execute.Common;
 import execute.DateAndTime;
 import org.junit.Assert;
 import org.junit.Test;
@@ -10,15 +11,21 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
 import java.time.LocalDate;
 
 
 public class DateAndTimeTest {
 
+
     @Test
     public void testValidDateAndTime() {
-        Assert.assertTrue(DateAndTime.isDateAndTimeValid("240531 9-17")); // YYMMDD format
+    	DateAndTime instance = new DateAndTime();
+
+        Assert.assertTrue(instance.isDateAndTimeValid("240531 9-17")); // YYMMDD format
     }
 
     @Test
@@ -38,7 +45,9 @@ public class DateAndTimeTest {
     
     @Test
     public void testInvalidDateAndTime4() {
-        Assert.assertFalse(DateAndTime.isDateAndTimeValid("2a0420 9-17")); // Valid date
+
+        Assert.assertFalse(DateAndTime.isDateAndTimeValid("2a0420 9-17")); // InValid date
+
     }
    
     
@@ -47,6 +56,57 @@ public class DateAndTimeTest {
         Assert.assertFalse(DateAndTime.isDateAndTimeValid("240030 9-17")); // Invalid date
     }
     
+
+    @Test
+    public void testInvalidDateAndTime6() {
+        Assert.assertTrue(DateAndTime.isDateAndTimeValid("240830 9-17")); // Valid date
+    }
+    
+    @Test
+    public void testInvalidDateAndTimeA() {
+        Assert.assertFalse(DateAndTime.isDateAndTimeValid("240840 9-17")); // InValid date
+    }
+
+    @Test
+    public void testInvalidDateAndTimeB() {
+        Assert.assertFalse(DateAndTime.isDateAndTimeValid("240940 9-17")); // InValid date
+    }
+    
+    @Test
+    public void testInvalidDateAndTimeC() {
+        Assert.assertTrue(DateAndTime.isDateAndTimeValid("240228 9-17")); // Valid date
+    }
+    
+    @Test
+    public void testInvalidDateAndTimeD() {
+        Assert.assertFalse(DateAndTime.isDateAndTimeValid("230240 9-17")); // InValid date
+    }
+    
+    @Test
+    public void testInvalidDateAndTimeF() {
+        Assert.assertFalse(DateAndTime.isDateAndTimeValid("234023 9-17")); // InValid date
+    }
+    
+    @Test
+    public void testInvalidDateAndTime7() {
+        Assert.assertFalse(DateAndTime.isDateAndTimeValid("240930 -8-10")); // Valid hour
+    }
+    
+    @Test
+    public void testInvalidDateAndTime8() {
+        Assert.assertFalse(DateAndTime.isDateAndTimeValid("240930 -8-30")); // Valid hour
+    }
+    
+    @Test
+    public void testInvalidDateAndTime9() {
+        Assert.assertTrue(DateAndTime.isDateAndTimeValid("240930 8-8")); // InValid hour
+    } 
+    
+    @Test
+    public void testInvalidDateAndTime10() {
+        Assert.assertFalse(DateAndTime.isDateAndTimeValid("240930 8-25")); // InValid date
+    }
+
     
     @Test
     public void testInvalidDateAndTime_InvalidTime() {
@@ -62,6 +122,12 @@ public class DateAndTimeTest {
     @Test
     public void testInvalidDateAndTime_WrongDay() {
         Assert.assertFalse(DateAndTime.isDateAndTimeValid("240230 9-17")); // Invalid day in February
+    }
+
+    
+    @Test
+    public void testInvalidDateAndTime_RightDay() {
+        Assert.assertTrue(DateAndTime.isDateAndTimeValid("230220 9-17")); // valid day in February
     }
 
    
@@ -102,5 +168,11 @@ public class DateAndTimeTest {
         Assert.assertFalse(DateAndTime.isDateValid("2405-31")); // Invalid format
     }
     
+    
+    
+
+    
+    
 
 }
+
